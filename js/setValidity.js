5a334f8dd59fba7a1bde02bd9697dc9258656dfc
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2017-01-14 18:17:18 -0500

    updating some styles and functionality

---
+++ js/setValidity.js
@@ -0,0 +1,13 @@
+
+var email = document.getElementsByClassName("email")[0];
+var yourName = document.getElementsByClassName("your-name")[0];
+var message = document.getElementsByClassName("textarea")[0];
+
+email.addEventListener('keyup', function(){
+    console.log('email');
+    if(email.validity.typeMismatch){
+        email.setCustomValidity("This is not an email, please provide a valid email!");
+    } else {
+        email.setCustomValidity("");
+    }
+});
