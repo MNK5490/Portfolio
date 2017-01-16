1237dbe65894106b376c1634632b794a222938c5
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2017-01-16 16:01:05 -0500

    triaging this form issue

--- js/setValidity.js
+++ js/setValidity.js
@@ -1,18 +1,24 @@
-var forms = document.getElementsByTagName('form')[0];
 var email = document.getElementsByClassName("email")[0];
 var yourName = document.getElementsByClassName("your-name")[0];
 var message = document.getElementsByClassName("textarea")[0];
-var inputArray = [email, yourName, message];
+var required = [email, yourName, message];
+var form = document.getElementsByClassName("form")[0];
 
-
-email.addEventListener("keyup", displayInvalidMessage(email, "Please provide a valid email"));
-yourName.addEventListener("keyup", displayInvalidMessage(yourName, "Please provide your name for the message"));
-message.addEventListener("keyup", displayInvalidMessage(message, "Please include some content for your message"));
-
-function displayInvalidMessage(element, message) {
-    if(element.checkValidity() === false){
-        element.setCustomValidity(message);
+email.addEventListener('keyup', function(){
+    console.log('email');
+    if(email.validity.typeMismatch){
+        email.setCustomValidity("This is not an email, please provide a valid email!");
     } else {
-        element.setCustomValidity('');
+        email.setCustomValidity("");
     }
-};
+});
+
+form.addEventListener("submit", function(event){
+    for(var i = 0; required.length > i; i++){
+            if (required[i].checkValidity() === false){
+                event.preventDefault;
+                console.log(required[i] + "is not valid!!!");
+                debugger;
+            }
+        }
+});
