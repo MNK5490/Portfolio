a0cb4eec7d8fdb73ed36c5497208f8259db44d80
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2017-01-23 08:59:40 -0500

    testing on-going for form validation is safari

--- js/setValidity.js
+++ js/setValidity.js
@@ -1,7 +1,8 @@
 var email = document.getElementsByClassName("email")[0];
 var yourName = document.getElementsByClassName("your-name")[0];
 var message = document.getElementsByClassName("textarea")[0];
-var required = [email, yourName, message];
+var subject = document.getElementsByClassName("textarea")[0];
+var required = [email, yourName, subject, message];
 var form = document.getElementsByClassName("form")[0];
 
 email.addEventListener('keyup', function(){
@@ -13,14 +14,15 @@ email.addEventListener('keyup', function(){
     }
 });
 
-form.addEventListener("submit", function(event){
-    for(var i = 0; required.length > i; i++){
-            if (required[i].checkValidity() === false){
+form.addEventListener("submit", function(event) {
+            if (required[0].checkValidity() === false) {
                 event.preventDefault();
-                console.log(required[i] + "is not valid!!!");
-                debugger;
+                console.log(required[0] + "is not valid!!!");
+                window.alert("Please ensure the email input field is filled out properly with a valid email address prior to submitting the form. Thank you!");
+            } else if (required[1,2,3].checkValidity() === false) {
+                event.preventDefault();
+                window.alert("Please ensure each field is filled out in the form prior to submitting. Thank you!");
             } else {
                 console.log("form is valid");
             }
-        }
 });
