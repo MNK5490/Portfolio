46ed2cb6d7e11f98f5ad0124934e07224d66a429
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2017-01-23 08:10:58 -0500

    testing form validation in safari

--- js/setValidity.js
+++ js/setValidity.js
@@ -1,6 +1,8 @@
 var email = document.getElementsByClassName("email")[0];
 var yourName = document.getElementsByClassName("your-name")[0];
 var message = document.getElementsByClassName("textarea")[0];
+var required = [email, yourName, message];
+var form = document.getElementsByClassName("form")[0];
 
 email.addEventListener('keyup', function(){
     console.log('email');
@@ -9,4 +11,14 @@ email.addEventListener('keyup', function(){
     } else {
         email.setCustomValidity("");
     }
+});
+
+form.addEventListener("submit", function(event){
+    for(var i = 0; required.length > i; i++){
+            if (required[i].checkValidity() === false){
+                event.preventDefault;
+                console.log(required[i] + "is not valid!!!");
+                debugger;
+            }
+        }
 });
