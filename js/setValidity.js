b5836d0fef3ce64b68b6046cf7bf85a85515d7d2
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2017-01-23 08:08:21 -0500

    going back to the old form validation

--- js/setValidity.js
+++ js/setValidity.js
@@ -1,8 +1,6 @@
 var email = document.getElementsByClassName("email")[0];
 var yourName = document.getElementsByClassName("your-name")[0];
 var message = document.getElementsByClassName("textarea")[0];
-var required = [yourName, email, message];
-var form = document.getElementsByClassName("form")[0];
 
 email.addEventListener('keyup', function(){
     console.log('email');
@@ -11,14 +9,4 @@ email.addEventListener('keyup', function(){
     } else {
         email.setCustomValidity("");
     }
-});
-
-form.addEventListener("submit", function(event){
-    for(var i = 0; required.length > i; i++){
-            if (required[i].checkValidity() === false){
-                event.preventDefault;
-                console.log(required[i] + "is not valid!!!");
-                debugger;
-            }
-        }
 });
