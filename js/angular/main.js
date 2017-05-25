9dec38b75fee49a48d79b1f909ea1b8d4aeedc8f
Maxwell Kendall <maxwell.n.kendall@gmail.com>
2017-05-25 18:30:54 -0400

    fixin a mistake when making it angularized

--- js/angular/main.js
+++ js/angular/main.js
@@ -47,38 +47,91 @@ max.controller('mainController', ['$scope', '$log', 'scroll', function($scope, $
       $scope.pageVariables.resume.style.border = "2px solid white";
       $scope.pageVariables.resume.style.background = "#009688";
     };
-    $scope.x = document;
-    document.addEventListener('scroll', scroll.hideScroll)
+    document.addEventListener('scroll', scroll.hideScroll);
   }]);
 
-max.controller('portfolioController', ['$scope', 'scroll', function($scope){
+max.controller('portfolioController', ['$scope', function($scope){
+    $scope.portfolioArray = [{
+            title: 'Web Dashboard',
+            url: 'https://maxwellkendall.github.io/Web-Dashboard/',
+            description: 'Chart.js library integration with client side local-storage and form-validation',
+            img: 'images/webdashboard.jpg'
+        },
+        {
+            title: 'Artist Portfolio',
+            url: 'https://ckendallart.com',
+            description: 'Shopify store customized according to Artist\'s requests',
+            img: 'images/ckendallart.jpg'
+        },
+        {
+            title: '508 Compliant Front End Refactor',
+            url: 'https://maxwellkendall.github.io/Accessibility/',
+            description: 'Refactored 508 Compliant Front End: includes tables, forms, and static pages',
+            img: 'images/accessible.jpg'
+        },
+        {
+            title: 'Custom Made Video Player',
+            url: 'https://maxwellkendall.github.io/VideoPlayer/',
+            description: 'Plain JavaScript with control panel, captions, and transcript made from scratch',
+            img: 'images/videoplayer.jpg'
+        },
+        {
+            title: 'Photo Gallery and Lightbox',
+            url: 'https://maxwellkendall.github.io/Photo-Gallery',
+            description: 'Photo gallery lightbox made from jQuery with search functionality',
+            img: 'images/Photo-Gallery.jpg'
+        },
+        {
+            title: 'SVG Design',
+            url: 'https://maxwellkendall.github.io/SVG-Project',
+            description: 'Responsive SVG\'s customized with customized colors',
+            img: 'images/SVGs.jpg'
+        },
+        {
+            title: 'Online Form',
+            url: 'https://maxwellkendall.github.io/Online-Form',
+            description: 'Online Form with custom styles',
+            img: 'images/Online-Form.jpg'
+        },
+    ];
+    console.log($scope.portfolioArray);
+}]);
+
+max.controller('experienceController', ['$scope', function($scope){
     $scope.test = [
         {test: 'abc'},
         { test: 'xyz'}
     ];
-    document.addEventListener('scroll', scroll.hideScroll);
 }]);
 
-max.controller('experienceController', ['$scope', 'scroll', function($scope){
-    $scope.test = [
-        {test: 'abc'},
-        { test: 'xyz'}
-    ];
-    document.addEventListener('scroll', scroll.hideScroll);
-}]);
+max.controller('contactController', ['$scope', function($scope){
+    var self = this;
+    $scope.variables = {
+        email : document.getElementsByClassName("email")[0],
+        yourName : document.getElementsByClassName("your-name")[0],
+        message : document.getElementsByClassName("textarea")[0],
+        subject : document.getElementsByClassName("textarea")[0],
+        required : [self.email, self.yourName, self.subject, self.message],
+        form : document.getElementsByClassName("form")[0]
+    }
+    $scope.validate = function(){
+        if($scope.variables.email.validity.typeMismatch){
+            $scope.variables.email.setCustomValidity("Please provide your real email. Thank you!");
+        } else {
+            $scope.variables.email.setCustomValidity("");
+        }
+    }
+    // $scope.variables.yourName.setCustomValidity('Please provide your name in the message!');
+    // $scope.variables.subject.setCustomValidity('Please provide a subject in the message!');
+    // $scope.variables.message.setCustomValidity('Please include a message prior to sending!');
 
-max.controller('contactController', ['$scope', 'scroll', function($scope){ $scope.test = [
-        {test: 'abc'},
-        { test: 'xyz'}
-    ];
-    document.addEventListener('scroll', scroll.hideScroll);
 }]);
 
 max.service('scroll', function() {
   var self = this;
   self.main_nav = document.getElementsByClassName('main-nav')[0];
   self.hideScroll = function(){
-  	if(window.scrollY >= 58 && window.innerWidth < 746){
+  	if(window.scrollY >= 58){
           console.log(window.scrollY);
           self.main_nav.style.visibility = 'hidden';
   } else if (window.scrollY === 0){
@@ -86,4 +139,15 @@ max.service('scroll', function() {
   		    self.main_nav.style.visibility = 'visible';
   	}
   };
+});
+
+max.directive('portfolioItem', function(){
+    return {
+        restrict: 'AECM',
+        replace: true,
+        templateUrl: "js/directives/portfolioItem.html",
+        scope: {
+            portfolioArray : "="
+        }
+    }
 });
